6eeb5892a904373912fd5b4c6fd6c2fb3c558a15
Davi Serrano <daviscardoso2901@gmail.com>
2022-10-07 00:13:13 -0300

    Começando com o JWT token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import { authenticateRoutes } from "./authenticate.routes"
 
 import { usersRoutes } from "./users.routers"
 
-const router = Router()
+const router = Router();
 
-router.use("/users", usersRoutes)
+router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
